7707cb5e5e06964c71b911bc0031a1ce8f2b1ba0
Patrick Lin <patricklin@Patricks-MBP.lan>
2024-08-03 15:51:00 -0400

    Adding team transfer lists

--- soccer-api-project/soccer_standings/transfers/urls.py
+++ soccer-api-project/soccer_standings/transfers/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("league/<int:pk>", views.team_list, name="team-list")
+    path("league/<int:pk>", views.team_list, name="team-list"),
+    path("team/<int:team_id>", views.team_transfers, name="team-transfers")
 ]
--- soccer-api-project/soccer_standings/transfers/views.py
+++ soccer-api-project/soccer_standings/transfers/views.py
@@ -13,6 +13,20 @@ def team_list(request, pk):
         'x-rapidapi-host': 'v3.football.api-sports.io'
     }
 
-    teams = requests.get(url, headers=headers).json().get('response')
-    teams.sort()
+    response = requests.get(url, headers=headers).json().get('response')
+    teams = [info['team'] for info in response]
+    teams.sort(key=lambda team: team['name'])
     return render(request, "transfers/team_list.html", {'teams': teams})
+
+def team_transfers(request, team_id):
+    url = "https://v3.football.api-sports.io/transfers?team={team_id}".format(team_id=team_id)
+
+    headers = {
+        'x-rapidapi-key': '9d4202b218ff9bc5640d6d2694bfc35a',
+        'x-rapidapi-host': 'v3.football.api-sports.io'
+    }
+    
+    transfers = requests.get(url, headers=headers).json().get('response')
+    transfers.sort(key=lambda transfer: transfer['transfers'][0]['date'], reverse=True)
+    return render(request, "transfers/team_transfers.html", {'team_id': team_id, 'transfers': transfers})
+
